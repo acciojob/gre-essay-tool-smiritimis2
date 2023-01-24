04fd6447f88c88096871068242884022d1de4363
smiritimis2 <113242412+smiritimis2@users.noreply.github.com>
2023-01-24 11:27:06 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
 //your code here
+let str;
+document.getElementById("evaluatedText").value = ""; 
+
+document.addEventListener("keyup", function () {
+	  str = document.getElementById("evaluatedText").value; 
+	// consostr.split(' ').length;
+		
+ let x = str.split(' ')
+	x= x.filter(item => item);
+	console.log(x)
+	document.querySelector("#wordCount").textContent = x.length;
+	
+    
+});
+
